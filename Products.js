ad94fac71805b2a5b25d635ce117cc65deca358e
Paula Robles <paulag.robles.sosa@gmail.com>
2021-02-25 22:40:33 -0600

    adding product model

---
+++ Products.js
@@ -0,0 +1,30 @@
+class Products{
+    constructor(id, full_Name, brand, description, category, price, inStock, image, createdAt, updatedAt){
+        this.id = id;
+        this.full_Name = full_Name; 
+        this.brand = brand; 
+        this.description = description; 
+        this.category = category; 
+        this.price = price; 
+        this.inStock = inStock; 
+        this.image = image;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    createProduct(){
+
+    }
+
+    getProducts(){
+
+    }
+
+    updateProduct(){
+
+    }
+
+    deleteProduct(){
+
+    }
+}
