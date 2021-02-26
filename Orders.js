4fc9b2379cf1fe7ff25c2953d81dcdc9e3c02b01
Alexis Garcia <alexgarciamo14@gmail.com>
2021-02-25 22:20:25 -0600

    Se hizo modelo Orders

---
+++ Orders.js
@@ -0,0 +1,25 @@
+class Orders {
+    constructor(User, OrderItems, ShippingAddress, paymentMethod, 
+                totalPrice, shippingPrice, isPaid, paidAt, isDelivered,
+                deliveredAt, createdAt, updatedAt)
+    {
+        this.User = User;
+        this.OrderItems = OrderItems;
+        this.ShippingAddress = ShippingAddress;
+        this.paymentMethod = paymentMethod;
+        this.totalPrice = totalPrice;
+        this.shippingPrice = shippingPrice;
+        this.isPaid = isPaid;
+        this.paidAt = paidAt;
+        this.isDelivered = isDelivered;
+        this.deliveredAt = deliveredAt;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    createOrder() {}
+    deleteOrder() {}
+    updateOrder() {}
+    getOrders() {}
+    
+}
