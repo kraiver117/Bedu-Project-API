3d4e12a4bb9c13f8d4f10ecedc7dcc9adeb5600b
Paula Robles <paulag.robles.sosa@gmail.com>
2021-03-13 03:24:52 -0600

    adding schema

--- models/Order.js
+++ models/Order.js
@@ -1,27 +1,62 @@
-class Order {
-    constructor(User, OrderItems, ShippingAddress, paymentMethod, 
-                totalPrice, shippingPrice, isPaid, paidAt, isDelivered,
-                deliveredAt, createdAt, updatedAt)
-    {
-        this.User = User;
-        this.OrderItems = OrderItems;
-        this.ShippingAddress = ShippingAddress;
-        this.paymentMethod = paymentMethod;
-        this.totalPrice = totalPrice;
-        this.shippingPrice = shippingPrice;
-        this.isPaid = isPaid;
-        this.paidAt = paidAt;
-        this.isDelivered = isDelivered;
-        this.deliveredAt = deliveredAt;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
+const mongoose = require('mongoose');
+//const bcrypt = require('bcryptjs');
+//const jwt = require('jsonwebtoken');
+const uniqueValidator = require("mongoose-unique-validator");
 
-    createOrder() {}
-    deleteOrder() {}
-    updateOrder() {}
-    getOrders() {}
-    
-}
+const OrderSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        required: [true, "no puede estar vacío"],
+        index = true,
+    },
+    OrderItems: {
+        type: String,
+        required: [true, "no puede estar vacío"],
 
-module.exports = Order;
+    },
+    ShippingAddress: {
+        type: String,
+        required: [true, "no puede estar vacío"], 
+    },
+    paymentMethod: {
+        required: [true, "no puede estar vacío"],
+    },
+    totalPrice: { Number },
+    shippingPrice: { Number },
+    isPaid: { Boolean },
+    paidAt: {
+        //
+    },
+    isDelivered: { Boolean },
+    deliveredAt: {
+        //
+    },
+    createdAt: {
+        //timestamps
+    },
+    updatedAt: {
+        //timestamps
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("Orders", OrderSchema);
+
+
+OrderSchema.methods.publicData = function(){
+    return{
+        User = this.User,
+        OrderItems = this.OrderItems,
+        ShippingAddress = this.ShippingAddress,
+        paymentMethod = this.paymentMethod,
+        totalPrice = this.totalPrice,
+        shippingPrice = this.shippingPrice,
+        isPaid = this.isPaid,
+        paidAt = this.paidAt,
+        isDelivered = this.isDelivered,
+        deliveredAt = this.deliveredAt,
+        createdAt = this.createdAt,
+        updatedAt = this.updatedAt,
+    };
+};
