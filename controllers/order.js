4a7dbe2245da6c1140ad2ca7132825ce85518190
Angel <gutierrezramosjoseangel@gmail.com>
2021-03-14 18:48:03 -0600

    Order Routes Added

--- controllers/order.js
+++ controllers/order.js
@@ -12,79 +12,79 @@ exports.getOrders = asyncHandler(async (req, res) => {
 // @desc      Create order
 // @route     POST /v1/orders
 // @access    private
-exports.createOrder = asyncHandler(async (req, res, next) =>  {
-    const {
+exports.createOrder = asyncHandler(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod
+  } = req.body;
+
+  if (!orderItems && orderItems.length === 0) {
+    return next(new ErrorResponse('No existen productos en la orden', 400));
+  } else {
+    const shippingPrice = 150;
+    const totalPrice = (orderItems.reduce((accumulator, item) => accumulator + item.qty * item.price, 0) + shippingPrice).toFixed(2);
+    const isPaid = paymentMethod ? true : false;
+
+    const order = new Order({
       orderItems,
+      user: req.user.id,
       shippingAddress,
-      paymentMethod
-    }=req.body
+      paymentMethod,
+      isPaid,
+      shippingPrice,
+      totalPrice
+    });
 
-    if(!orderItems && orderItems.length === 0){
-      return next(new ErrorResponse('No existen productos en la orden', 400));
-    }else{
-      const shippingPrice = 150;
-      const totalPrice = (orderItems.reduce((accumulator, item) => accumulator + item.qty * item.price, 0) + shippingPrice).toFixed(2);
-      const isPaid = paymentMethod ? true : false;
+    const createdOrder = await order.save();
 
-      const order = new Order({
-        orderItems, 
-        user: req.user.id,
-        shippingAddress,
-        paymentMethod,
-        isPaid,
-        shippingPrice,
-        totalPrice
-      });
-
-      const createdOrder = await order.save();
-
-      res.status(201).json({
-        success: true,
-        data: createdOrder
+    res.status(201).json({
+      success: true,
+      data: createdOrder
     });
   }
-})
+});
 
 
 // @desc      get order by ID 
 // @route     GET /v1/orders
 // @access    private
-exports.getOrderbyID = asyncHandler(async (req, res, next) =>  {
+exports.getOrderbyId = asyncHandler(async (req, res, next) => {
   const order = await Order.findById(req.params.id).populate('user');
-  
-  if(order){
+
+  if (order) {
     res.status(200).json({
       success: true,
       data: order
-    })
-  }else{
+    });
+  } else {
     return next(new ErrorResponse('No se encontró la orden', 404));
   }
-})
+});
 
 // @desc Get logged in user orders 
 //@route Get /v1/orders/myorders 
 //@access Private 
-exports.getMyOrders = asyncHandler(async (req, res, next) =>  {
+exports.getMyOrders = asyncHandler(async (req, res, next) => {
   const orders = await Order.find({ user: req.user.id });
-  
-  res.status(200).json({ success: true, data: orders});
-})
+
+  res.status(200).json({ success: true, data: orders });
+});
 
 // @desc Update order to delivered 
 //@route PUT /v1/orders/:id/deliver 
 //@access Private/admin 
-exports.updateOrderToDelivered = asyncHandler(async (req, res, next) =>  {
+exports.updateOrderToDelivered = asyncHandler(async (req, res, next) => {
   const order = await Order.findById(req.params.id);
 
-  if(order){
+  if (order) {
     order.isDelivered = true;
     order.deliveredAt = Date.now();
 
     const updatedOrder = await order.save();
 
-    res.status(200).json({ success: true, data: updatedOrder });    
-  }else{
+    res.status(200).json({ success: true, data: updatedOrder });
+  } else {
     return next(new ErrorResponse('No se encontró la orden', 404));
   }
-})
+});
